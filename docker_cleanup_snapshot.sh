84088984500c1c1211448fc8022715fe5c833884
prabhuanand <prabhuanand@users.noreply.github.com>
2016-11-09 09:44:24 -0800

    Update docker_cleanup_snapshot.sh

--- docker_cleanup_snapshot.sh
+++ docker_cleanup_snapshot.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-# This will have the last 2 built images.
+# This will have the last 2 built images for every docker image.
 
-docker images | grep -i SNAPSHOT | awk '{print $1}' | sort | uniq > /tmp/_snapshot
+docker images | awk '{print $1}' | sort | uniq > /tmp/_snapshot
 
 for i in `cat /tmp/_snapshot`
 do
 	echo "Cleaning up Docker image: $i"
-	docker images | grep -i SNAPSHOT | grep -i $i  | tail -n +3 | xargs docker rmi -f
+	docker images | grep -i $i  | tail -n +3 | xargs docker rmi -f
 done
